afef4d46a9c050638d34e4ee5e0e49a3e2fd5efe
Edison Ruiz <emruiz4@utpl.edu.ec>
2016-11-30 08:32:13 -0500

    Archivo

---
+++ ejemplos-java/ManejoArchivos/src/lecturaArchivos/LeerArchivoTexto.java
@@ -0,0 +1,79 @@
+package lecturaArchivos;
+
+// Ejemplo tomado del libro 
+// D EITEL , P AUL J. Y H ARVEY M. D EITEL
+// CÓMO PROGRAMAR EN J AVA . Séptima edición
+// fines educativos
+// Prueba de la clase ArchivoTexto.
+
+// Este programa lee un archivo de texto y muestra cada registro.
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.lang.IllegalStateException;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class LeerArchivoTexto {
+
+    private Scanner entrada;
+
+    // permite al usuario abrir el archivo
+    public void abrirArchivo() {
+        try {
+            entrada = new Scanner(new File("datos.txt"));
+        } // fin de try
+        catch (FileNotFoundException fileNotFoundException) {
+            System.err.println("Error al abrir el archivo.");
+            System.exit(1);
+        } // fin de catch
+    } // fin del método abrirArchivo
+
+    // lee registro del archivo
+    public void leer_informacion() {
+        int sumas=0;
+        int suma_total=0;
+        int valor = 0;
+        int valores=0;
+        int Valores2[] = new int[9];
+        try // lee registros del archivo, usando el objeto Scanner
+        {
+            while (entrada.hasNext()) {
+                String linea = entrada.nextLine();
+                 //System.out.println(linea);
+                ArrayList<String> linea_partes = new ArrayList<String>(Arrays.asList(linea.split(";")));
+                valor = Integer.parseInt(linea_partes.get(0));
+                
+                for (int j = 0; j < linea_partes.size(); j++) {
+                    valores = Integer.parseInt(linea_partes.get(j));
+                    suma_total = suma_total + valores;
+                    
+                }
+                sumas = sumas + valor;
+                System.out.println(linea_partes);
+
+            } // fin de while
+            System.out.println("La suma de valores es: "+sumas);
+            System.out.println("La suma total es: "+suma_total);
+        } // fin de try
+        catch (NoSuchElementException elementException) {
+            System.err.println("El archivo no esta bien formado.");
+            entrada.close();
+            System.exit(1);
+        } // fin de catch
+        catch (IllegalStateException stateException) {
+            System.err.println("Error al leer del archivo.");
+            System.exit(1);
+        } // fin de catch
+    } // fin del metodo leerRegistros
+    // cierra el archivo y termina la aplicación
+
+    public void cerrarArchivo() {
+        if (entrada != null) {
+            entrada.close(); // cierra el archivo
+        }
+    } // fin del metodo cerrarArchivo
+} // fin de la clase LeerArchivoTexto
+
